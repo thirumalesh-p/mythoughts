58f389ff845b0603e8e245fcd5a0355c8d5d528f
thiru <thirumalesh.pottum@gmail.com>
2020-04-05 23:33:46 +0530

    second commit

---
+++ mythoughts/src/main/java/com/mythoughts/MythoughtsApplication.java
@@ -0,0 +1,13 @@
+package com.mythoughts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythoughtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythoughtsApplication.class, args);
+	}
+
+}
